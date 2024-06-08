e6a168c9050fec53aa0d4d71b5937fb037c6869b
Marina Moroz <moroz2387moroz@yandex.ru>
2024-06-08 16:53:30 +1000

    ad functions using async/await

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,23 @@
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('start')
-})
+window.addEventListener("DOMContentLoaded", () => {
+  const jokeEl = document.getElementById("joke");
+  const jokeBts = document.getElementById("jokeBts");
 
+  //using then
+  //   const generateJoke = () => {
+  //     fetch("https://v2.jokeapi.dev/joke/Any?type=single").then((res) =>
+  //       res.json().then((data) => {
+  //         jokeEl.innerHTML = data.joke;
+  //       })
+  //     );
+  //   };
+
+  //using async/await
+  const generateJoke = async () => {
+    const res = await fetch("https://v2.jokeapi.dev/joke/Any?type=single");
+    const data = await res.json();
+    jokeEl.innerHTML = data.joke;
+  };
+  generateJoke();
+
+  jokeBts.addEventListener("click", generateJoke);
+});
